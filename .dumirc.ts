d1c0c26418676e211a97392a15303fd8332fe232
紫竹 <luozhu2021@gmail.com>
2024-07-23 14:37:17 +0800

    chore: 允许 client 为空

--- .dumirc.ts
+++ .dumirc.ts
@@ -29,6 +29,6 @@ export default defineConfig({
   exportStatic: {},
   ...(process.env.NODE_ENV === 'development' ? {} : { ssr: {} }),
   headScripts: [
-    {src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', async: true, crossorigin: 'anonymous'},
+    {src: '/adsbygoogle.js', async: true, crossorigin: 'anonymous'},
   ]
 });
